fdbe218d0e3bba145673698ded085ded58e7d706
Esad Dursun <3723esad@gmail.com>
2022-04-28 02:02:35 +0200

    8kyu - reversed strings

---
+++ 8kyu/reversed_strings.js
@@ -0,0 +1,10 @@
+// INSTRUCTIONS
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// SOLUTION
+function solution(string) {
+  return string.split("").reverse().join("");
+}
