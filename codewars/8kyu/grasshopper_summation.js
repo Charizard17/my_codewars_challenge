45275db9934d35761bced0ca92a0fd47dec255f3
Esad Dursun <3723esad@gmail.com>
2022-04-30 01:19:43 +0200

    codewars 8kyu - grasshopper summation

---
+++ codewars/8kyu/grasshopper_summation.js
@@ -0,0 +1,23 @@
+// INSTRUCTIONS
+// Summation
+// Write a program that finds the summation of every number from 1 to num.
+// The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+// SOLUTION
+// For this problem I will create a mathematical equation that makes the summation
+
+// solution without looping
+
+var summation = function (num) {
+    return (num + 1) * (num / 2);
+  }
+  
+  // time complexity is O(1)
