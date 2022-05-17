6851318038e82c1e7a0a363bf700d7edf1dff5bc
Esad Dursun <3723esad@gmail.com>
2022-05-17 21:37:09 +0200

    codewars 7kyu - sum of two lowest integers

---
+++ codewars/7kyu/sum_of_two_lowest_integers.js
@@ -0,0 +1,25 @@
+// INSTRUCTIONS
+// Sum of two lowest positive integers
+
+// Create a function that returns the sum of the two lowest positive
+// numbers given an array of minimum 4 positive integers. No floats
+// or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the
+// output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// SOLUTION
+// no need to validate input numbers array
+
+// sort array
+
+// return sum of zero and first indides values
+
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort((a, b) => a - b);
+  return numbers[0] + numbers[1];
+}
+
+// time complexity is O(n)
