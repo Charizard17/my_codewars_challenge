02871436922034b25d53fdfe6d5ecfc358154321
Esad Dursun <3723esad@gmail.com>
2022-05-19 17:23:48 +0200

    codewars 8kyu - returning strings

---
+++ codewars/8kyu/returning_strings.js
@@ -0,0 +1,15 @@
+// INSTRUCTIONS
+// Returning Strings
+
+// Make a function that will return a greeting statement that uses an input;
+// your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// SOLUTION
+// convert input type to string
+// return the greeting statement
+
+function greet(name) {
+  return "Hello, " + name.toString() + " how are you doing today?";
+}
