541ab3647cfe4858d02da46eb5020b545304c819
Esad Dursun <3723esad@gmail.com>
2022-05-24 21:25:26 +0200

    codewars 7kyu - friend or foe

---
+++ codewars/7kyu/friend_or_foe.js
@@ -0,0 +1,26 @@
+// INSTRUCTIONS
+// Friend or Foe?
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+// SOLUTION
+// input is a list of strings
+
+// filter the list by their length
+
+// return the filtered list same order like input list
+
+function friend(friends) {
+  return friends.filter((friend) => friend.length == 4);
+}
+
+// time complexity is O(n)
