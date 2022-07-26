e3cf970ded73b36668224c755a53344a5a0ba6e3
Esad Dursun <3723esad@gmail.com>
2022-07-26 08:05:19 +0200

    codewars 8kyu - function 1 hello world

---
+++ codewars/8kyu/function_1_hello_world.js
@@ -0,0 +1,22 @@
+// INSTRUCTIONS
+// Function 1 - hello world
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the
+// most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+// SOLUTION
+// be careful about names of variable, function, etc!
+
+// simple arrow function would work for this problem
+
+// Write a function "greet" that returns "hello world!"
+let greet = () => "hello world!";
